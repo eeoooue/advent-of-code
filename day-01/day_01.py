3b5b842e6ce9211778c1f93c3734a834645b642c
556pm <pdmoorhouse@gmail.com>
2022-12-14 13:01:29 +0000

    day 01 refactoring

--- day-01/day_01.py
+++ day-01/day_01.py
@@ -1,16 +1,7 @@
 
-import os
+from filereader import FileReader
 
-os.chdir("D:\\GitHub\\advent-of-code\\day-01")
-
-array = []
-
-with open("input.txt", "r") as file:
-    for line in file:
-        print(line[:-1])
-        array.append(line[:-1])
-        
-print(array)
+array = FileReader.get_lines("01", "input.txt")
 
 food = []
 total = 0
@@ -22,46 +13,7 @@ for value in array:
         total += int(value)
 food.append(total)
 
-most = max(food)
+food.sort(reverse=True)
 
-def solve_part1(arr):
-
-    n = len(arr)
-
-    best = 0
-    ans = 0
-    for i, x in enumerate(arr):
-        if x >= best:
-            best = x
-            ans = i
-
-    
-
-    return best
-
-
-def solve_part2(food):
-
-    copy = food[:]
-    copy.sort()
-
-    print(copy)
-
-    total = 0
-    for i in range(3):
-        total += copy.pop()
-
-    return total
-
-answer1 = solve_part1(food)
-
-print(f"Part 1 = {answer1}")
-
-answer2 = solve_part2(food)
-
-
-print(f"Part 2 = {answer2}")
-
-
-
-
+print(f"Part 1 = {food[0]} (75501 for me)")
+print(f"Part 2 = {sum(food[:3])} (215594 for me)")
